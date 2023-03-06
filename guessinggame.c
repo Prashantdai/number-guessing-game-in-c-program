76cadae9cc5a991f4bcdd7e641f2e2f575344232
PRASHANT <126753289+Prashantdai@users.noreply.github.com>
2023-03-06 12:32:55 +0545

    Add files via upload

---
+++ guessinggame.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+	int a;
+	int b;
+	printf("enter two numbers ");
+	scanf("%d",&a,&b);
+	
+	call function avg(a,b){
+		c=(a+b)/2;
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
